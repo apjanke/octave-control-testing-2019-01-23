a7cd49c576a3f4aa04675f1500befb223a8cd366
Andrew Janke <floss@apjanke.net>
2019-01-24 07:18:58 -0500

    Prevent timeout on Travis when brewing from source

--- dev-tools/install-octave-macos.sh
+++ dev-tools/install-octave-macos.sh
@@ -2,6 +2,7 @@
 
 set -x
 
+bottle=0
 if [[ "$OCTAVE_BLAS" == "openblas" ]]; then
 	if [[ "$OCTAVE_VER" == "stable" ]]; then
 		formula=octave-stable-openblas
@@ -19,18 +20,25 @@ else
 		formula=octave-default
 	elif [[ "$OCTAVE_VER" == "4.4" ]]; then
 		formula=octave
+		bottle=1
 	else
 		echo &>2 Unsupported OCTAVE_VER: $OCTAVE_VER. Must be stable, default, or 4.4
 	fi
 fi
 
 echo "Installing brew Octave formula: $formula"
-brew install "$formula" --without-docs
+if [[ $bottle == 1 ]]; then
+	brew install "$formula"
+else
+	# Gotta use --verbose to prevent Travis from timing out
+	brew install --verbose "$formula" --without-docs
+fi
 ln -s $(brew --prefix "$formula")/bin/octave ~/bin
 ln -s $(brew --prefix "$formula")/bin/mkoctfile ~/bin
 
 
 echo "Octave installation results:"
+brew info "$formula"
 which octave
 ls -l $(which octave)
 which mkoctfile
